0ea8095a9e8e51a2f8017a88994afa56b2654699
gpaoloni <paolonigianfranco@gmail.com>
2019-11-01 13:06:48 -0300

    Mongoose Part 1

---
+++ node_mongoose/index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log('Connected correctly to the server');
+
+  let newDish = Dishes({
+    name: 'Uthappizza',
+    description: 'test',
+  })
+
+  newDish.save()
+    .then( (dish) => {
+      console.log(dish);
+
+      return Dishes.find({}).exec();
+    })
+    .then( (dishes) => {
+      console.log(dishes);
+
+      return Dishes.deleteMany({})
+    })
+    .then( (result) => {
+      console.log('DeleteMany result: ', result);
+
+      return mongoose.connection.close();
+    })
+    .catch( (err) => {
+      console.log(err);
+    });
+});
