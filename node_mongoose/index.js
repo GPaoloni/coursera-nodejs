e096ac84119f7cee5b9988a58d95b76cd703db40
gpaoloni <paolonigianfranco@gmail.com>
2019-11-06 13:11:22 -0300

    Mongoose part 2

--- node_mongoose/index.js
+++ node_mongoose/index.js
@@ -8,28 +8,33 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
   console.log('Connected correctly to the server');
 
-  let newDish = Dishes({
+
+  // let newDish = Dishes({
+  //   name: 'Uthappizza',
+  //   description: 'test',
+  // })
+
+  // newDish.save()
+  Dishes.create({
     name: 'Uthappizza',
     description: 'test',
   })
+  .then( (dish) => {
+    console.log(dish);
 
-  newDish.save()
-    .then( (dish) => {
-      console.log(dish);
+    return Dishes.find({}).exec();
+  })
+  .then( (dishes) => {
+    console.log(dishes);
 
-      return Dishes.find({}).exec();
-    })
-    .then( (dishes) => {
-      console.log(dishes);
+    return Dishes.deleteMany({})
+  })
+  .then( (result) => {
+    console.log('DeleteMany result: ', result);
 
-      return Dishes.deleteMany({})
-    })
-    .then( (result) => {
-      console.log('DeleteMany result: ', result);
-
-      return mongoose.connection.close();
-    })
-    .catch( (err) => {
-      console.log(err);
-    });
+    return mongoose.connection.close();
+  })
+  .catch( (err) => {
+    console.log(err);
+  });
 });
