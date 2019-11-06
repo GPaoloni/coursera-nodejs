920670023f2b289f395f4efbf48a7d0c68febfa8
gpaoloni <paolonigianfranco@gmail.com>
2019-11-06 13:40:34 -0300

    Addes subdocument to dishSchema

--- node_mongoose/index.js
+++ node_mongoose/index.js
@@ -8,13 +8,6 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
   console.log('Connected correctly to the server');
 
-
-  // let newDish = Dishes({
-  //   name: 'Uthappizza',
-  //   description: 'test',
-  // })
-
-  // newDish.save()
   Dishes.create({
     name: 'Uthappizza',
     description: 'test',
@@ -22,12 +15,27 @@ connect.then((db) => {
   .then( (dish) => {
     console.log(dish);
 
-    return Dishes.find({}).exec();
+    return Dishes.findByIdAndUpdate(dish._id, { 
+      $set: { description: 'Updated test' }
+    }, {
+      new: true
+    }).exec();
   })
-  .then( (dishes) => {
-    console.log(dishes);
+  .then( (dish) => {
+    console.log(dish);
 
-    return Dishes.deleteMany({})
+    dish.comments.push({
+      rating: 5,
+      comment: 'Tasty as heck',
+      author: 'Leonardo'
+    });
+
+    return dish.save();
+  })
+  .then( (dish) => {
+    console.log(dish);
+
+    return Dishes.deleteMany({});
   })
   .then( (result) => {
     console.log('DeleteMany result: ', result);
